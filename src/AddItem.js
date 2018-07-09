d883efb45e1b8a841d93574d87afec5fb7837824
Ty Mondragon <tymondragon@gmail.com>
2018-07-09 16:16:56 -0600

    getting exercise 4 to work

---
+++ src/AddItem.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class AddItem extends Component {
+  constructor (props){
+    super(props)
+  }
+
+  onChange = (e) => {
+    e.preventDefault()
+    this.setState({[e.target.className]: e.target.value})
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.addToCart(this.state)
+  }
+
+  render () {
+  let productItems = this.props.products.map(item => <option key={item.id} value={this.props.name}>{item.name}</option>)
+
+  return (
+      <div className="container">
+          <div className="row">
+            <form className="form-inline" onSubmit ={ this.onSubmit }>
+              <div className="row">
+                <label>
+                  Quantity:
+                  <input type="number" id="quantity" onChange={ this.onChange } className="quantity"/>
+                </label>
+                <label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">Products</label>
+                <select className="products" onChange={ this.onChange }>
+                  { productItems }
+                </select>
+              </div>
+              <button className="btn btn-primary" type="submit">Button</button>
+            </form>
+          </div>
+        </div>
+      )
+  }
+}
+
+export default AddItem
--- src/App.js
+++ src/App.js
@@ -4,13 +4,39 @@ import './App.css';
 import CartHeader from './CartHeader.js';
 import CartFooter from './CartFooter.js';
 import CartItems, { cartItemsList } from './CartItems.js';
+import { products } from './products.js'
+import AddItem from './AddItem.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state= { products: [
+      { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+      { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+      { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+      { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+      { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+      { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+      { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+      { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+      { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+    ]}
+  }
+
+  addToCart = (newItem) => {
+    let array = this.state.products.filter(product => newItem.products === product.name)
+    let productObj = {
+        id:1,
+        product: result[0],
+        quantity: newItem.quantity
+  }
+
   render() {
     return (
       <div>
         <CartHeader />
         <CartItems items= { cartItemsList }/>
+        <AddItem products={ this.state.products } addToCart={ this.addToCart }/>
         <CartFooter copyright=" 2018"/>
       </div>
     );
